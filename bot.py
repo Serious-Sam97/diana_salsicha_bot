8ccf1c24376dc9f6cc6b331341d043fc68d3ac7d
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2024-02-13 22:54:04 -0300

    Added .env support and OpenAI support

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import telebot
+import openai
+from dotenv import load_dotenv
+import os
 
-bot = telebot.TeleBot("6741867245:AAFZxpRbhRrteB-KiOVAWaXU3C32JnCvqc0");
+load_dotenv();
+
+openai.api_key = os.getenv('OPENAI_API_KEY');
+bot = telebot.TeleBot(os.getenv('TELEBOT_API_KEY'));
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Ahoy Sir, how can I help?");
-
+    bot.reply_to(message, "Ahoy Sir, how can I help?");
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	# bot.reply_to(message, message.text)
-	bot.reply_to(message, "WHAAAAT")
-
+    # bot.reply_to(message, message.text)
+    bot.reply_to(message, "WHAAAAT")
 bot.infinity_polling()
